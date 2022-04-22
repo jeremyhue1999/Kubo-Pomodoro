2d9fb253d94a57cf245dc7e6fffd9343b3de5b0d
Carlojara36 <63653923+Carlojara36@users.noreply.github.com>
2022-04-22 20:20:46 +0800

    Added env.js

---
+++ src/env.js
@@ -0,0 +1,6 @@
+export const API_KEY = ""
+export const AUTH_DOMAIN = ""
+export const PROJECT_ID = ""
+export const STORAGE_BUCKET = ""
+export const MESSAGING_SENDER_ID = ""
+export const APP_ID = ""
