7270445f20ce6b7e9ddf2d8a4211dc739ab21a5c
LawrenceAllen <bolanos.allenlawrence@gmail.com>
2022-04-22 14:02:28 +0800

    Created server folder. Installed express, firebase, and cors. Initialized server files

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+
+const serviceAccount = require('./service-key.json');
+
+initializeApp({
+  credential: cert(serviceAccount)
+});
+
+const db = getFirestore();
