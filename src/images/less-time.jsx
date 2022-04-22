2742371493c542ae407fcd6841525873288efd24
LawrenceAllen <bolanos.allenlawrence@gmail.com>
2022-04-22 17:00:19 +0800

    Deleted server folder

--- src/images/less-time.jsx
+++ src/images/less-time.jsx
@@ -11,8 +11,8 @@ export default function LessTime({ fill, hoverFill, className }) {
       onMouseOut={() => setSvgFill(fill)}
     >
       <svg viewBox="0 0 42 36" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M18 34C9.16344 34 2 26.8366 2 18C2 9.16344 9.16344 2 18 2" stroke={svgFill} stroke-width="4"/>
-        <path d="M18 34C26.8366 34 34 26.8366 34 18" stroke={svgFill}  stroke-width="4"/>
+        <path d="M18 34C9.16344 34 2 26.8366 2 18C2 9.16344 9.16344 2 18 2" stroke={svgFill} strokeWidth="4"/>
+        <path d="M18 34C26.8366 34 34 26.8366 34 18" stroke={svgFill}  strokeWidth="4"/>
         <rect x="18" width="4" height="4" fill={svgFill}/>
         <rect x="32" y="16" width="4" height="2" fill={svgFill}/>
         <rect x="16" y="9" width="3" height="12" fill={svgFill}/>
